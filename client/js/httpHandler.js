2cb81690c674b98f391f9d1a4126934a30506296
gchen1196 <gchen1196@gmail.com>
2019-04-26 18:54:17 -0700

    implemented request to server

--- client/js/httpHandler.js
+++ client/js/httpHandler.js
@@ -1,49 +1,57 @@
-(function() {
+const serverUrl = 'http://127.0.0.1:3000';
 
-  const serverUrl = 'http://127.0.0.1:3000';
+//
+// TODO: build the swim command fetcher here
+//
 
-  //
-  // TODO: build the swim command fetcher here
-  //
-
-  /////////////////////////////////////////////////////////////////////
-  // The ajax file uplaoder is provided for your convenience!
-  // Note: remember to fix the URL below.
-  /////////////////////////////////////////////////////////////////////
-
-  const ajaxFileUplaod = (file) => {
-    var formData = new FormData();
-    formData.append('file', file);
-    $.ajax({
-      type: 'POST',
-      data: formData,
-      url: 'FILL_ME_IN',
-      cache: false,
-      contentType: false,
-      processData: false,
-      success: () => {
-        // reload the page
-        window.location = window.location.href;
-      }
-    });
-  };
-
-  $('form').on('submit', function(e) {
-    e.preventDefault();
-
-    var form = $('form .file')[0];
-    if (form.files.length === 0) {
-      console.log('No file selected!');
-      return;
+const getSwimCommand = () => {
+  $.ajax({
+    type: 'GET',
+    url: `${serverUrl}/commands`,
+    success: (res) => {
+      console.log(res);
+      SwimTeam.move(res);
     }
+  })
+}
+// setInterval(getSwimCommand, 500);
+/////////////////////////////////////////////////////////////////////
+// The ajax file uplaoder is provided for your convenience!
+// Note: remember to fix the URL below.
+/////////////////////////////////////////////////////////////////////
 
-    var file = form.files[0];
-    if (file.type !== 'image/jpeg') {
-      console.log('Not a jpg file!');
-      return;
+const ajaxFileUplaod = (file) => {
+  var formData = new FormData();
+  formData.append('file', file);
+  $.ajax({
+    type: 'POST',
+    data: formData,
+    url: 'FILL_ME_IN',
+    cache: false,
+    contentType: false,
+    processData: false,
+    success: () => {
+      // reload the page
+      window.location = window.location.href;
     }
-
-    ajaxFileUplaod(file);
   });
+};
 
-})();
+$('form').on('submit', function(e) {
+  e.preventDefault();
+
+  var form = $('form .file')[0];
+  if (form.files.length === 0) {
+    console.log('No file selected!');
+    return;
+  }
+
+  var file = form.files[0];
+  if (file.type !== 'image/jpeg') {
+    console.log('Not a jpg file!');
+    return;
+  }
+
+  ajaxFileUplaod(file);
+});
+
--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -8,7 +8,14 @@ module.exports.backgroundImageFile = path.join('.', 'background.jpg');
 ////////////////////////////////////////////////////////
 
 module.exports.router = (req, res, next = ()=>{}) => {
+
   console.log('Serving request type ' + req.method + ' for url ' + req.url);
   res.writeHead(200, headers);
+  console.log(req);
+  if (req.method === 'GET' && req.url === '/commands') {
+    const options = ['left', 'right', 'up', 'down'];
+    const index = Math.floor(Math.random() * 4);
+    res.end(options[index]);
+  }
   res.end();
 };
